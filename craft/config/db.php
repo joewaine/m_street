c8eca591a29878cf2d361391e030d8eb63cc411a
Joe Waine <joseph@comradeagency.com>
2017-08-29 12:09:42 -0700

    mamnoon street

---
+++ craft/config/db.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+
+return array(
+
+	'*' => array(
+	),
+
+
+
+	 'localhost' => array(
+
+    'server' => 'localhost',
+	  'database' => 'mamnoon_street',
+	  'user' => 'root',
+	  'password' => 'root',
+	  'tablePrefix' => 'craft',
+      ),
+
+);
+
+
+
+
+
