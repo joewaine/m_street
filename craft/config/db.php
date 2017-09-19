3a8a518a2720c2d954e80a68e805054933e2d15b
Joe Waine <joseph@comradeagency.com>
2017-09-19 13:02:00 -0700

    db fix

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,25 +7,50 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
+// return array(
+
+// 	'*' => array(
+// 	),
+
+
+
+// 	 'localhost' => array(
+
+//     'server' => 'localhost',
+// 	  'database' => 'mamnoon_street',
+// 	  'user' => 'root',
+// 	  'password' => 'root',
+// 	  'tablePrefix' => 'craft',
+//       ),
+
+// );
+
+
 return array(
 
-	'*' => array(
-	),
+        '*' => array(
+        ),
+        'testsite.mamnoon.webfactional.com' => array(
 
-
-
-	 'localhost' => array(
-
-    'server' => 'localhost',
-	  'database' => 'mamnoon_street',
-	  'user' => 'root',
-	  'password' => 'root',
-	  'tablePrefix' => 'craft',
+       // The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+        'server' => 'localhost',
+        // The name of the database to select.
+        'database' => 'mamnoon_street_2017_install',
+        // The database username to connect with.
+        'user' => 'mbar_webmaster',
+        // The database password to connect with.
+        'password' => 'Seattle98109Mbar',
+        // The prefix to use when naming tables. This can be no more than 5 characters.
+        'tablePrefix' => 'craft'
+        ),
+       'localhost' => array(
+          'server' => 'localhost',
+          'database' => 'mamnoon_street',
+          'user' => 'root',
+          'password' => 'root',
+          'tablePrefix' => 'craft',
       ),
 
 );
-
-
-
 
 
